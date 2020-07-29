4c3ff5d4a761c500a2beb27764b73cabb4fd3657
rajsekhar240 <rajsekharmahapatro@gmail.com>
2020-07-29 14:58:13 +0530

    promisify the ajax call

--- Ajax Js 1/app.js
+++ Ajax Js 1/app.js
@@ -4,30 +4,38 @@ const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
 const img = document.querySelector(".container img");
 btn.addEventListener("click", () => {
-  getData(url);
+  getData(url)
+    .then((response) => displayData(response))
+    .catch((response) => console.log(response));
 });
 
 function getData(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.send();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) {
-      return;
-    }
-    if (xhr.status === 200) {
-      img.classList.add("shake-img");
-      const { value: joke } = JSON.parse(xhr.responseText);
-      const balayya = joke.replace(/Chuck Norris/, "Balayya");
-      content.textContent = balayya;
-      setTimeout(() => {
-        img.classList.remove("shake-img");
-      }, Math.random() * 1000);
-    } else {
-      console.log({
-        status: xhr.status,
-        message: status.statusText,
-      });
-    }
-  };
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject({
+          status: xhr.status,
+          message: status.statusText,
+        });
+      }
+    };
+  });
+}
+
+function displayData() {
+  img.classList.add("shake-img");
+  const { value: joke } = JSON.parse(xhr.responseText);
+  const balayya = joke.replace(/Chuck Norris/, "Balayya");
+  content.textContent = balayya;
+  setTimeout(() => {
+    img.classList.remove("shake-img");
+  }, Math.random() * 1000);
 }
